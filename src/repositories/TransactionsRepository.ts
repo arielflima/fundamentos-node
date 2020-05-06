2a7057c245b5588404d0191351e1013c2e9a77a8
Ariel V Lima <arielflima@gmail.com>
2020-05-06 15:50:40 -0300

    finishing challange

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,45 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce(
+      (transactionIncome, transaction) => {
+        if (transaction.type === 'income') {
+          return transactionIncome + transaction.value;
+        }
+
+        return transactionIncome;
+      },
+      0,
+    );
+
+    const outcome = this.transactions.reduce(
+      (transactionOutcome, transaction) => {
+        if (transaction.type === 'outcome') {
+          return transactionOutcome + transaction.value;
+        }
+
+        return transactionOutcome;
+      },
+      0,
+    );
+
+    return {
+      income,
+      outcome,
+      total: income - outcome,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransaction): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
